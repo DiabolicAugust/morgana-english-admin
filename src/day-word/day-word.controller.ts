6b7ff48557d2b40944b043743d56f3268b4ecd21
DiabolicAugust <griffliway@gmail.com>
2024-04-14 16:33:56 +0300

    Database setup. Word of day functionality

---
+++ src/day-word/day-word.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { DayWordService } from './day-word.service.js';
+import { CreateDayWordDto } from './dto/create-day-word.dto.js';
+
+@Controller('day-word')
+export class DayWordController {
+  constructor(private readonly dayWordService: DayWordService) {}
+  @Post()
+  @UsePipes(ValidationPipe)
+  async create(@Body() dto: CreateDayWordDto) {
+    return this.dayWordService.create(dto);
+  }
+
+  @Post('/createMany')
+  @UsePipes(ValidationPipe)
+  async createMany(@Body('content') content: [CreateDayWordDto]) {
+    return this.dayWordService.createMany(content);
+  }
+
+  @Get()
+  async getRandom() {
+    return this.dayWordService.getOneRandom();
+  }
+}
---
+++ src/day-word/day-word.service.ts
@@ -0,0 +1,59 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import {
+  DayWord,
+  DayWordDocument,
+} from 'morgana-english-shared/dist/models/day-word.model.js';
+import { CreateDayWordDto } from './dto/create-day-word.dto.js';
+import { Strings } from '../data/strings.js';
+import { Models } from '../data/enums.js';
+
+@Injectable()
+export class DayWordService {
+  constructor(
+    @InjectModel(DayWord.name)
+    private readonly dayWordModel: Model<DayWordDocument>,
+  ) {}
+  async create(dto: CreateDayWordDto) {
+    console.log(await this.dayWordModel.create({ history: 'sss' }));
+    const dayWord = await this.dayWordModel.create(dto);
+    if (!dayWord)
+      throw new HttpException(
+        Strings.modelNotCreated(Models.DayWord),
+        HttpStatus.BAD_REQUEST,
+      );
+    return dayWord.save();
+  }
+
+  async createMany(content: CreateDayWordDto[]) {
+    try {
+      // Вставка нових документів, ігноруючи дублікати
+      const words = await this.dayWordModel.insertMany(content, {
+        ordered: false,
+      });
+      return words;
+    } catch (error) {
+      // Обробка помилок, пов'язаних з дублікатами
+      if (error.code === 11000) {
+        // Логіка для обробки помилок дублікатів
+        console.error('Some duplicate words were ignored');
+      } else {
+        // Логіка для обробки інших помилок
+        throw error;
+      }
+    }
+  }
+
+  async getOneRandom() {
+    const dayWord = await this.dayWordModel.findOne({ used: false });
+    if (!dayWord)
+      throw new HttpException(
+        Strings.noUnusedDayWordsLeft,
+        HttpStatus.NOT_FOUND,
+      );
+
+    await dayWord.updateOne({ used: true });
+    return dayWord;
+  }
+}
