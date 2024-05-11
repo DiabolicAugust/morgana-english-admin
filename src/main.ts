54fe8353333e0f023e999ee248fc81a336eabc96
DiabolicAugust <griffliway@gmail.com>
2024-05-11 15:57:18 +0300

    Implemented cors for front

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://morgana-english-front.onrender.com',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+
   await app.listen(8080);
 }
 bootstrap();
